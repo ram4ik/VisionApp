a3e4e85b1dc4d30221d5b0b494ded6932fe7ddb7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-03-26 18:12:59 +0200

    Initial Commit

---
+++ VisionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisionApp
+//
+//  Created by ramil on 26.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VisionApp/VisionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionAppApp.swift
+//  VisionApp
+//
+//  Created by ramil on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct VisionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
